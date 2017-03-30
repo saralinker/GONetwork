b47182e6fc536e8ed7c68c8b10867fddcbdb50c8
saralinker <sara.linker@gmail.com>
2017-03-30 13:20:55 -0700

    .

--- R/ancestorcount.R
+++ R/ancestorcount.R
@@ -1,23 +1,23 @@
-ancestorcount <- function(x, ancestor) {length(ancestor[[x]])}
-
-require(GO.db)
-
-
-xx <- as.list(GOBPANCESTOR)
-gos <- names(xx)
-#bpancestors <- lapply(X = gos, FUN = ancestorcount, ancestor = GOBPANCESTOR)
-bpanc <- data.frame(go = gos,  bp = unlist(bpancestors),row.names = gos)
-
-xx <- as.list(GOCCANCESTOR)
-gos <- names(xx)
-#ccancestors <- lapply(X = gos, FUN = ancestorcount, ancestor = GOCCANCESTOR)
-ccanc <- data.frame(go = gos,  bp = unlist(ccancestors),row.names = gos)
-
-xx <- as.list(GOMFANCESTOR)
-gos <- names(xx)
-#mfancestors <- lapply(X = gos, FUN = ancestorcount, ancestor = GOMFANCESTOR)
-mfanc <- data.frame(go = gos,  bp = unlist(mfancestors),row.names = gos)
-
-
-#save(list = c("bpanc","ccanc","mfanc"), file = "~/Documents/R/packages/GONetwork/R/sysdata.rda",compress = TRUE)
-
+# ancestorcount <- function(x, ancestor) {length(ancestor[[x]])}
+# 
+# require(GO.db)
+# 
+# 
+# xx <- as.list(GOBPANCESTOR)
+# gos <- names(xx)
+# #bpancestors <- lapply(X = gos, FUN = ancestorcount, ancestor = GOBPANCESTOR)
+# bpanc <- data.frame(go = gos,  bp = unlist(bpancestors),row.names = gos)
+# 
+# xx <- as.list(GOCCANCESTOR)
+# gos <- names(xx)
+# #ccancestors <- lapply(X = gos, FUN = ancestorcount, ancestor = GOCCANCESTOR)
+# ccanc <- data.frame(go = gos,  bp = unlist(ccancestors),row.names = gos)
+# 
+# xx <- as.list(GOMFANCESTOR)
+# gos <- names(xx)
+# #mfancestors <- lapply(X = gos, FUN = ancestorcount, ancestor = GOMFANCESTOR)
+# mfanc <- data.frame(go = gos,  bp = unlist(mfancestors),row.names = gos)
+# 
+# 
+# #save(list = c("bpanc","ccanc","mfanc"), file = "~/Documents/R/packages/GONetwork/R/sysdata.rda",compress = TRUE)
+# 
