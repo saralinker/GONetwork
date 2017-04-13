3257a1fb152809460599aebf9e5792737eed0666
saralinker <sara.linker@gmail.com>
2017-04-13 12:28:32 -0700

    find terms cols not rows

--- R/findTerms.R
+++ R/findTerms.R
@@ -13,7 +13,8 @@ findTerms <- function(M,genes = NULL, proportion.shared = 1){
   
   terms <- M[genes,]
   if(length(genes) > 1){
-    terms <- names(terms[(rowSums(terms) / length(genes)) >= proportion.shared,])
+    terms[terms > 0] <- 1
+    terms <- names(terms[,(colSums(terms) / length(genes)) >= proportion.shared])
   }else{
     terms <- names(terms[terms > 0])
   }
